4ea8d775b0d464c07d4f9f75286f9e36505e4a62
m.shabana <m.shabana@index-soft.com>
2020-09-30 11:31:38 +0200

    add swager

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -3,6 +3,14 @@ import { ItemsService } from './items.service';
 import { CreateItemdto } from './dto/creat.items.deto';
 import { Items } from './interface/items.interface';
 
+import {
+    ApiBearerAuth,
+    ApiOperation,
+    ApiResponse,
+    ApiTags
+  } from '@nestjs/swagger';
+
+@ApiTags('items')
 @Controller('items')
 export class ItemsController {
     constructor(
@@ -11,6 +19,8 @@ export class ItemsController {
 
     //send Data 
     @Post('additem')
+    @ApiOperation({summary: "add item to DB"})
+    @ApiResponse({status:200, description: "Done"})
     async Additems( @Body()itemDataDto: CreateItemdto ):Promise<Items>{
         return await this._ItemsService.insertData(itemDataDto);
     }
@@ -18,11 +28,15 @@ export class ItemsController {
 
     //return Data 
     @Get(':parent')
+    @ApiOperation({summary: "get parent "})
+    @ApiResponse({status:200, description: "Done"})
     async getParentonly(@Param()parent: string):Promise<Items[]>{
         return await this._ItemsService.findParent(parent);
     }
 
     @Get(':allparents')
+    @ApiOperation({summary: "get All under Parent"})
+    @ApiResponse({status:200, description: "Done"})
     async getallunderParent(@Param()allparents: string):Promise<Items[]>{
         return await this._ItemsService.findallunderParent(allparents);
     }
@@ -31,6 +45,8 @@ export class ItemsController {
 
     // Delete
     @Delete(':id')
+    @ApiOperation({summary: "Delete Item from DB"})
+    @ApiResponse({status:200, description: "Done"})
     async deleteParent(@Param()id: string) : Promise<Items> {
         return await this._ItemsService.deleteItem(id);
     }
